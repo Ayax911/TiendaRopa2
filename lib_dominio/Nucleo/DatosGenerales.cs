78b2c1d11ae6684152ca296911f6efefd47ae853
TiendaRopa <j.mast8924@gmail.com>
2025-04-01 12:10:14 -0500

    Pruebas unitarias

--- lib_dominio/Nucleo/DatosGenerales.cs
+++ lib_dominio/Nucleo/DatosGenerales.cs
@@ -2,9 +2,8 @@
 {
     public class DatosGenerales
     {
-        public static string ruta_json = @"C:\Users\Ojeda\OneDrive\Desktop\RamaJulian\secretos.json";
+        public static string ruta_json = @"C:\Users\Ojeda\OneDrive\Desktop\TiendaRopa2\secretos.json";
         public static bool usa_azure = false;
         public static string clave = "EVBgi345936456ghhVBJGtgnifytsidi3456678jhgUTytutyiiyi";
         public static string usuario_datos = EncriptarConversor.Encriptar("Test.Trghhjsgdj");
     }
-}
---
+++ ut_presentacion/Nucleo/EntidadesNucleo.cs
@@ -0,0 +1,41 @@
+﻿
+using lib_dominio.Entidades;
+using lib_repositorios.Interfaces;
+
+namespace ut_presentacion.Nucleo
+{
+    public class EntidadesNucleo
+    {
+        public static Clientes? Clientes()
+        {
+            var entidad = new Clientes();
+            entidad.Cedula = "Pruebas-" + DateTime.Now.ToString("yyyyMMddhhmmss");
+            entidad.Nombre = "Sujeto Prueba 2";
+            entidad.Telefono = "3105254987";
+            return entidad;
+        }
+
+        
+            public static Marcas? Marcas()
+            {
+                var entidad = new Marcas();
+                entidad.Nit = "222222";
+                entidad.Nombre = "Marca Prueba 2";
+
+                return entidad;
+            }
+
+        public static Productos? Productos(IConexion conexion)
+        {
+            var marca = conexion.Marcas!.FirstOrDefault(x => x.Nombre == "RopaFina");
+
+            var entidad = new Productos();
+            entidad.Nombre = "ArturoCalle";
+            entidad.Material = "Lino";
+            entidad.ValorUnitario = 100000;
+            entidad.Marca = marca!.Id;
+
+            return entidad;
+        }
+    }
+}
