38dd5f3d8099e09766bc31d5de494a89d08812c1
TiendaRopa <j.mast8924@gmail.com>
2025-04-01 21:53:20 -0500

    Prueba unitaria Compras

--- lib_dominio/Entidades/Compras.cs
+++ lib_dominio/Entidades/Compras.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace lib_dominio.Entidades
 {
 
@@ -11,16 +13,20 @@ namespace lib_dominio.Entidades
         public decimal ValorTotal { get; set; }
 
         //FK
-        public int Clientes { get; set; }
-        public int Sucursales { get; set; }
-        public int MetodosPagos { get; set; }
-        public int Lugares { get; set; }
+        public int Cliente { get; set; }
+        public int Sucursal { get; set; }
+        public int MetodoPago { get; set; }
+        public int Lugar { get; set; }
 
-
-        public Clientes? _Cliente { get; set; }
+        /*public Clientes? _Cliente { get; set; }
         public Sucursales? _Sucursal { get; set; }
         public MetodosPagos? _MetodoPago { get; set; }
-        public Lugares? _Lugar { get; set; }
+        public Lugares? _Lugar { get; set; }*/
+
+        [ForeignKey("Cliente")] public Clientes? _Cliente { get; set; }
+        [ForeignKey("Sucursal")] public Sucursales? _Sucursal { get; set; }
+        [ForeignKey("MetodoPago")] public MetodosPagos? _MetodoPago { get; set; }
+        [ForeignKey("Lugar")] public Lugares? _Lugar { get; set; }
 
 
         public List<DetallesCompras>? DetallesCompras { get; set; }
--- ut_presentacion/Nucleo/EntidadesNucleo.cs
+++ ut_presentacion/Nucleo/EntidadesNucleo.cs
@@ -67,5 +67,25 @@ namespace ut_presentacion.Nucleo
             return entidad;
         }
 
+        public static Compras? Compras(IConexion conexion)
+        {
+            var cliente = conexion.Clientes!.FirstOrDefault(x => x.Id == 1);
+            var sucursal = conexion.Sucursales!.FirstOrDefault(x => x.Id == 1);
+            var metodoPago = conexion.MetodosPagos!.FirstOrDefault(x => x.Id == 1);
+            var lugar = conexion.Lugares!.FirstOrDefault(x => x.Id == 1);
+
+            var entidad = new Compras();
+            entidad.Fecha = DateTime.Now;
+            entidad.Codigo = "Compras Prueba";
+            entidad.ValorTotal = 10000.05m;
+
+            entidad.Cliente = cliente!.Id;
+            entidad.Sucursal = sucursal!.Id;
+            entidad.MetodoPago = metodoPago!.Id;
+            entidad.Lugar = lugar!.Id;
+
+            return entidad;
+        }
+
     }
 }
